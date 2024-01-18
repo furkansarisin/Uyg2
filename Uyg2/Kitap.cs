2a6d3d9d4dec2feb45bb5247afd15830e0f03c1e
Furkan Sarışın <Furkan Sarn@DESKTOP-DQN25QC>
2024-01-18 20:06:14 +0300

    Proje dosyası ekle.

---
+++ Uyg2/Kitap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg2
+{
+    
+    class Kitap
+    {
+        public string adi;
+        public string yazari;
+        static int oduncKitap = 0;
+
+
+        public void oduncVer()
+        {
+            Console.WriteLine(yazari + "tarafindan yazilan " + adi +
+                " isimli kitap odun verildi.");
+            oduncKitap++;
+            Console.WriteLine("Odunc verilen kitap sayısı: " + oduncKitap);
+            
+        }
+        public static void statikMetot()
+        {
+            Console.WriteLine("Ben bir statik metodum");
+            
+            
+
+        }
+
+    }
+}
---
+++ Uyg2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap k1 = new Kitap();
+            k1.adi = "Fareler ve Insanlar";
+            k1.yazari = "John Steinbeck";
+            k1.oduncVer();
+
+            Kitap k2 = new Kitap();
+            k2.adi = "Hayvan Ciftligi";
+            k2.yazari = "George Orwell";
+            k2.oduncVer();
+
+            Kitap.statikMetot();
+        }
+    }
+}
